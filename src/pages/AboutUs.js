cc8f9ea2bed57387f62a83ad21df0b58e173c674
sagarpatole113 <sagarpatole113@gmail.com>
2023-03-04 13:04:38 +0530

    first commit

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,84 @@
+import './AboutUs.css'
+const AboutUs =()=>{
+    return (
+        <div >
+            <table>
+                <tr >
+                    <td></td>
+                    <td >
+                    <img src="images/shubham.jpeg" alt="img" className="pic" />
+                    &nbsp;
+                    &nbsp;
+                    Shubham Nikam
+                    </td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                   <td></td>
+                   <td></td>
+                   <td></td>
+                    <td>
+                    <img src="images/wajid.jpeg"alt="img" className="pic" />
+                    &nbsp;
+                    &nbsp;
+                    Wajid Mulla
+                    </td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                    <img src="images/sagar.jpeg"alt="img" className="pic" />
+                    &nbsp;
+                    &nbsp;
+                    Sagar Patole
+                    </td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                    <img src="images/MohammadFarooq.JPG"alt="img" className="pic" />
+                    &nbsp;
+                    &nbsp;
+                    Mohammad Farooq
+                    </td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                <td colspan="6" className="center">
+                        <a href="https://www.facebook.com">
+                            <img className="logo2" src="https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png?w=512&h=512" alt=""/>
+                        </a>
+                        |
+                        <a href="https://www.instagram.com/">
+                            <img className="logo2" src="images/insta.svg" alt=""/>
+                        </a>
+                        |
+                        <a href="https://twitter.com/">
+                            <img className="logo2" src="images/twitter.jpg" alt=""/>
+                        </a>
+              </td>
+              </tr>
+            </table>
+
+        </div>
+          
+      )
+}
+
+export default AboutUs
